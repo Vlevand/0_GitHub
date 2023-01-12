14e5ff8d392a1e0e3f85a58a8136810c8f0248a9
vlevand <viktoriya_levand@mail.ru>
2023-01-13 05:31:31 +0900

    Added solution to task 8

---
+++ Task08/Program.cs
@@ -0,0 +1,18 @@
+﻿/* Напишите программу, которая на вход
+принимает число (N), а на выходе показывает все чётные
+числа от 1 до N.
+5 -> 2, 4
+8 -> 2, 4, 6, 8 */
+
+Console.Clear();
+Console.Write("Введите целое положительное число от 2: ");
+int x = Convert.ToInt32(Console.ReadLine()), count = 2;
+if (x < 2) Console.Write("Ошибка! Введены некорректные данные.");
+else
+{
+    while (count <= x)
+    {
+    Console.Write(count+" ");
+    count +=2;
+    }
+}
