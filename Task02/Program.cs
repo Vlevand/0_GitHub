07397950d31755c8e01c114c73340aebea2836f2
v.levand <viktoriya_levand@mail.ru>
2023-01-19 06:55:08 +0900

    Added correction to task 2

--- Task02/Program.cs
+++ Task02/Program.cs
@@ -10,11 +10,19 @@ Console.Write("Введите первое целое число: ");
 int a = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите второе целое число: ");
 int b = Convert.ToInt32(Console.ReadLine());
-if (a > b)
+if (a == b)
 {
-    Console.Write($" Число {a} максимальное.");
+    Console.Write($"Числа {a} и {b} равны.");
 }
 else
 {
-  Console.Write($"Число {b} максимальное.");
+
+    if (a > b)
+    {
+        Console.Write($" Число {a} максимальное.");
+    }
+    else
+    {
+        Console.Write($"Число {b} максимальное.");
+    }
 }
