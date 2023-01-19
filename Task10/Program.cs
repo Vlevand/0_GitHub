134593ebdb8d8ea22e8d901ce90e647e4b1faa1b
v.levand <viktoriya_levand@mail.ru>
2023-01-19 16:31:55 +0900

    Added solution to task 10

---
+++ Task10/Program.cs
@@ -0,0 +1,21 @@
+﻿/* Напишите программу, которая принимает на
+вход трёхзначное число и на выходе показывает вторую
+цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1 */
+
+int SecondDigit(int number)
+{
+    if (number < 0) number = -number;
+    int result = number / 10 % 10;
+    return result;
+}
+
+Console.Write("Введите трехзначное число: ");
+int digit = Convert.ToInt32(Console.ReadLine());
+
+if ((digit > 99 && digit < 1000) || (digit > -1000 && digit < -99))
+Console.WriteLine($"Второе число в числе {digit}: {SecondDigit(digit)}");
+else Console.WriteLine("Ошибка! Введено не трехзначное число.");
