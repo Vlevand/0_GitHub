8f658ca44f10c5c77bb24523d0c3691e167f14ba
v.levand <viktoriya_levand@mail.ru>
2023-01-19 13:34:15 +0900

    Added solution to task 14

---
+++ Task14/Program.cs
@@ -0,0 +1,25 @@
+﻿/* Напишите программу, которая принимает на
+вход число и проверяет, кратно ли оно
+одновременно 7 и 23.
+14 -> нет
+46 -> нет
+161 -> да */
+
+string Multiplicity(int number)
+{
+    string result;
+    if (number % 7 == 0 && number % 23 == 0)
+    {
+        result = $"Число {number} кратно 7 и 23";
+    }
+    else
+    {
+        result = $"Число {number} не кратно одновременно 7 и 23";
+    }
+    return result;
+}
+
+Console.Write("Введите число: ");
+int digit = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(Multiplicity(digit));
