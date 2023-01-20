76084481e240c96777a95b61a14cfaf28e02239b
v.levand <viktoriya_levand@mail.ru>
2023-01-21 01:28:34 +0900

    Added solution to task 22

---
+++ Task22/Program.cs
@@ -0,0 +1,19 @@
+﻿/* Напишите программу, которая
+принимает на вход число (N) и выдаёт таблицу
+квадратов чисел от 1 до N. */
+
+Console.Write("Введите натуральное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n > 1)
+{
+    Console.WriteLine("N -> Sqrt(N)");
+    for (int i = 1; i <= n; i++)
+        Console.WriteLine(TableSqrt(i));
+}
+else Console.Write("Ошибка!!! Введено не натуральное число.");
+
+string TableSqrt(int x)
+{
+    return Convert.ToString(x) + "   " + Convert.ToString(x * x);
+}
